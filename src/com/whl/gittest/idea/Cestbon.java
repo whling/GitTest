1145dd1261998530cdbf9ace67f198647924befe
whling <1769778747@qq.com>
2017-06-16 12:16:45 +0800

    idea 整合github,修改

---
+++ src/com/whl/gittest/idea/Cestbon.java
@@ -0,0 +1,13 @@
+package com.whl.gittest.idea;
+
+/**
+ * @Author: Whling
+ * @Date: Created in 12:15 2017/6/16
+ * @Modified By:
+ * @Description:
+ */
+public class Cestbon {
+    public static void main(String[] args) {
+
+    }
+}
