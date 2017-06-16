b0804dd8274e6862faae12552d8311c0b35c2f97
whling <1769778747@qq.com>
2017-06-16 11:21:33 +0800

    idea 整合github

---
+++ src/com/whl/gittest/idea/HelloGIt.java
@@ -0,0 +1,13 @@
+package com.whl.gittest.idea;
+
+/**
+ * @Author: Whling
+ * @Date: Created in 11:18 2017/6/16
+ * @Modified By:
+ * @Description:
+ */
+public class HelloGIt {
+    public static void main(String[] args) {
+        System.out.println("hello github,i am Intelij Idea");
+    }
+}
